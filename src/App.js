189a8c6bdf2b492068148fff32132cf6ef452d45
brian4418 <brian4418@naver.com>
2024-10-14 09:50:07 -0500

    Front-end Integration (Header, Nav + Homepage)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
+import Location from './components/Location';
 import PopularPlaces from './components/PopularPlaces';
 import NeerPlaces from './components/NeerPlaces';
+import FloatingButton from './components/FloatingButton';
+import Nav from './components/Nav';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Location />
             <PopularPlaces />
             <NeerPlaces />
+            <FloatingButton />
+            <Nav />
         </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Header.css';
-import gpsIcon from '../assets/gps-icon.png';
+import logo from '../assets/logos/pbs-wisconsin-wblue-rgb.png';
 
 function Header() {
-    const [city, setCity] = useState('City');
-    const [permissionDenied, setPermissionDenied] = useState(false);
-
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success, error);
-        } else {
-            alert('Geolocation is not supported by this browser.');
-        }
-    };
-
-    const success = position => {
-        const { latitude, longitude } = position.coords;
-
-        // Reverse geocoding to get city name
-        fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.address) {
-                    const { city, town, village } = data.address;
-                    setCity(city || town || village || 'Unknown Location');
-                } else {
-                    setCity('Unknown Location');
-                }
-            })
-            .catch(() => setCity('Unknown Location'));
-    };
-
-    const error = err => {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-        setPermissionDenied(true);
-        setCity('Permission Denied');
-    };
-
     return (
-        <div className="header">
-            <div className="location" onClick={getLocation}>
-                <img src={gpsIcon} alt="GPS Icon" />
-                <span>You're in <b>{city}</b></span>
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="Logo" />
             </div>
-            <h1><b>Discover</b></h1>
-            <h1>{city}</h1>
-        </div>
+        </header>
     );
 }
 
